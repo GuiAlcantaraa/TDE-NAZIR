2c22b55a464bd7a8089b6cf2146fae9c0fe62e49
Guilherme de Alcantara Machado <guilherme.machado@zema.com>
2022-11-29 15:29:04 -0300

    assets

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import logo from './assets/Logo-nlw-esports.svg'
 import { CreateAdBanner } from './components/CreateAdBanner'
 import { CreateAdModal } from './components/CreateAdModal'
 
-interface Game {
-  id: string;
-  title: string;
-  bannerUrl: string;
-  _count: {
-    ads: number;
-  }
-}
+import primeiro from '../src/assets/game-1.png'
+import segundo from '../src/assets/game-2.png'
+import terceiro from '../src/assets/game-3.png'
+import quarto from '../src/assets/game-4.png'
+import quinto from '../src/assets/game-5.png'
+import sexto from '../src/assets/game-6.png'
+
+
 
 function App() {
   return (
@@ -31,32 +31,32 @@ function App() {
       <div className='grid grid-cols-6 gap-6 mt-4'>
 
         <GameBanner
-          bannerUrl='../public/game-1.png'
+          bannerUrl={primeiro}
           title='League of Legends'
           adsCount={35}
         />
         <GameBanner
-          bannerUrl='../public/game-2.png'
+          bannerUrl={segundo}
           title='Dota 2'
           adsCount={20}
         />
         <GameBanner
-          bannerUrl='../public/game-3.png'
+          bannerUrl={terceiro}
           title='CSGO'
           adsCount={55}
         />
         <GameBanner
-          bannerUrl='../public/game-4.png'
+          bannerUrl={quarto}
           title='Apex legends'
           adsCount={13}
         />
         <GameBanner
-          bannerUrl='../public/game-5.png'
+          bannerUrl={quinto}
           title='Fortnite'
           adsCount={43}
         />
         <GameBanner
-          bannerUrl='../public/game-6.png'
+          bannerUrl={sexto}
           title='World WarCraft'
           adsCount={15}
         />
